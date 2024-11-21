70e0f4c6e83a6a6a6c05038aad1cd27805e0c607
VuHieu27-1 <vuhieu27082002@gmail.com>
2024-11-21 22:25:37 +0700

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ const config = {
   // tbServer: "212.38.94.144:8081",
   // tbServer: "212.38.94.144",
   // tbServer: "thingsboard.srv524038.hstgr.cloud",
-  deviceId: "3ZoaOF9sdx6xRs0v5rY0",
+  deviceId: "deeb0400-9d16-11ef-97bd-1f25be15eb58",
 };
 
 export { config };
